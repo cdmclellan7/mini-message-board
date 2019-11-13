d76af33143d89ece40d8d6c1dc129143ba937aef
cdmclellan7 <cdmclellan7@gmail.com>
2019-11-13 11:47:30 -0500

    Implemented routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {text: "Hi there!", user: "Armando", added: new Date()},
+  {text: "Hello World!", user: "Charles", added: new Date()}
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Create New Message'});
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.author, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
